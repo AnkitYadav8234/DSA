b8604b796664371f0ca350ffee0595faa111674d
AnkitYadav8234 <140303644+AnkitYadav8234@users.noreply.github.com>
2024-04-08 00:17:49 +0530

    Array Practice Done

---
+++ src/main/java/ArraysQuestion/MaxInArray.java
@@ -0,0 +1,11 @@
+package ArraysQuestion;
+
+import java.util.Scanner;
+
+public class MaxInArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+
+}
